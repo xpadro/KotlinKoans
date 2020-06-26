ea75eabba575aee979129dad14d6ec27fa18ec57
Xavier Padro <xpadro.sw@gmail.com>
2020-06-26 18:48:30 +0200

    default arguments

--- Introduction/Default arguments/src/Task.kt
+++ Introduction/Default arguments/src/Task.kt
@@ -1,4 +1,4 @@
-fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+fun foo(name: String, number: Int = 1, toUpperCase: Boolean = false) =
         (if (toUpperCase) name.toUpperCase() else name) + number
 
 fun useFoo() = listOf(
