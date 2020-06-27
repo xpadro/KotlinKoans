1f38243d7257b20250fea147b5017acea2193c86
Xavier Padro <xpadro.sw@gmail.com>
2020-06-27 10:54:47 +0200

    comparison

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,13 @@
+import java.time.LocalDate
+
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+
+    override fun compareTo(other: MyDate): Int {
+        val date1 = LocalDate.of(this.year, this.month, this.dayOfMonth)
+        val date2 = LocalDate.of(other.year, other.month, other.dayOfMonth)
+
+        return date1.compareTo(date2)
+    }
 }
 
 fun test(date1: MyDate, date2: MyDate) {
